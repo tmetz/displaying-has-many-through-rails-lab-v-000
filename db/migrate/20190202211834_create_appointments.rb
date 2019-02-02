4398bad26b27afc3dbfa39f3ed4ecaa1bf61b4e4
Tammy Metz <tammy@tammymetz.com>
2019-02-02 21:27:54 +0000

    Automatically backed up by Learn

--- db/migrate/20190202211834_create_appointments.rb
+++ db/migrate/20190202211834_create_appointments.rb
@@ -1,8 +1,8 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.datetime, :appointment_datetime
-      t.integer, :doctor_id
+      t.datetime :appointment_datetime
+      t.integer :doctor_id
       t.integer :patient_id
 
       t.timestamps null: false

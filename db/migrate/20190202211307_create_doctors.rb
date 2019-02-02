9aab74b2bf2c2497eb1bf45c133a10bc232b5879
Tammy Metz <tammy@tammymetz.com>
2019-02-02 21:27:34 +0000

    Automatically backed up by Learn

--- db/migrate/20190202211307_create_doctors.rb
+++ db/migrate/20190202211307_create_doctors.rb
@@ -1,7 +1,7 @@
 class CreateDoctors < ActiveRecord::Migration
   def change
     create_table :doctors do |t|
-      t.string, :name
+      t.string :name
       t.string :department
 
       t.timestamps null: false

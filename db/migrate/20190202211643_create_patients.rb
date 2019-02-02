b229c744b4a40c8851e2f2be1db1e30d1012af36
Tammy Metz <tammy@tammymetz.com>
2019-02-02 21:27:43 +0000

    Automatically backed up by Learn

--- db/migrate/20190202211643_create_patients.rb
+++ db/migrate/20190202211643_create_patients.rb
@@ -1,7 +1,7 @@
 class CreatePatients < ActiveRecord::Migration
   def change
     create_table :patients do |t|
-      t.string, :name
+      t.string :name
       t.integer :age
 
       t.timestamps null: false
